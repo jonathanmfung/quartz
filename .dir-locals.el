c68eb13408437abdd6a3bd9807e20d52c0f30f59
Jonathan Fung <jonathanfung2000@gmail.com>
2025-02-12 09:38:00 -0800

    add project dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((jf/project-compile-commands .
+				 (("build serve" . "npx quartz build --serve"))))))
